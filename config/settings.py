511d7f8cd8d508b276a7e90b76ca71102d0673c2
Invictus666 <waichung.ng@gmail.com>
2020-10-01 12:46:10 +0800

    Heroku deployment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_nroo(zq5@la$^_khzh1m58x^&s!ru0j@z-r#@!*tt7nc*)%jm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.herokuapp.com','localhost','127.0.0.1']
 
 
 # Application definition
